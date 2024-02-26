e66e25b0958f982341578faea4dfc099cb9ad37d
Reichard Cris Adem <reichardcris@gmail.com>
2024-02-26 08:16:01 +0800

    Add a ProviderScope child wrapper in void main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:blog_post_app/config/router_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(RouterView());
+void main() => runApp(
+  ProviderScope(child: RouterView())
+);
