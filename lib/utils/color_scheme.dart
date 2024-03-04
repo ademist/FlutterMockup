d551317e8435035370586fd30fd966fe2c7a80f4
Reichard Cris Adem <reichardcris@gmail.com>
2024-03-04 17:56:35 +0800

    added Color Scheme

---
+++ lib/utils/color_scheme.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+Map<int, Color> primaryColor = {
+  0: const Color(0xFF001E42),
+  10: const Color(0xFF093A75),
+  20: const Color(0xFF0D55AA),
+  30: const Color(0xFF0E68D2),
+  40: const Color(0xFF2288FF),
+  50: const Color(0xFF3E97FF),
+  60: const Color(0xFF56A4FF),
+  70: const Color(0xFF7AB7FF),
+  80: const Color(0xFF95C6FF),
+  90: const Color(0xFFBBDAFF),
+  95: const Color(0xFFD1E6FF),
+  99: const Color(0xFFEBF4FF),
+};
+
+// fromARGB
+
+Map<int, Color> secondary1Color = {
+  0: const Color.fromRGBO(12, 0, 46, 1),
+  10: const Color.fromRGBO(25, 0, 95, 1),
+  20: const Color.fromRGBO(36, 1, 135, 1),
+  30: const Color.fromRGBO(49, 6, 171, 1),
+  40: const Color.fromRGBO(60, 11, 201, 1),
+  50: const Color.fromRGBO(75, 22, 225, 1),
+  60: const Color.fromRGBO(96, 45, 239, 1),
+  70: const Color.fromRGBO(125, 80, 253, 1),
+  80: const Color.fromRGBO(159, 125, 255, 1),
+  90: const Color.fromRGBO(188, 165, 255, 1),
+  95: const Color.fromRGBO(222, 210, 255, 1),
+  99: const Color.fromRGBO(242, 237, 255, 1),
+};
+
+Map<int, Color> secondary2Color = {
+  0: const Color.fromRGBO(0, 34, 38, 1),
+  10: const Color.fromRGBO(0, 58, 66, 1),
+  20: const Color.fromRGBO(0, 96, 109, 1),
+  30: const Color.fromRGBO(5, 138, 156, 1),
+  40: const Color.fromRGBO(3, 176, 200, 1),
+  50: const Color.fromRGBO(0, 205, 233, 1),
+  60: const Color.fromRGBO(36, 229, 255, 1),
+  70: const Color.fromRGBO(86, 235, 255, 1),
+  80: const Color.fromRGBO(135, 241, 255, 1),
+  90: const Color.fromRGBO(172, 245, 255, 1),
+  95: const Color.fromRGBO(204, 249, 255, 1),
+  99: const Color.fromRGBO(233, 252, 255, 1),
+};
+
+Map<int, Color> errorColor = {
+  0: const Color.fromRGBO(195, 20, 20, 1),
+  10: const Color.fromRGBO(216, 32, 32, 1),
+  20: const Color.fromRGBO(255, 197, 197, 1),
+  30: const Color.fromRGBO(255, 216, 216, 1),
+};
+
+Map<int, Color> successColor = {
+  0: const Color.fromRGBO(0, 183, 51, 1),
+  10: const Color.fromRGBO(0, 216, 60, 1),
+  20: const Color.fromRGBO(98, 252, 141, 1),
+  30: const Color.fromRGBO(185, 255, 204, 1),
+};
+
+Map<int, Color> warningColor = {
+  0: const Color.fromRGBO(220, 158, 0, 1),
+  10: const Color.fromRGBO(255, 189, 20, 1),
+  20: const Color.fromRGBO(255, 215, 114, 1),
+  30: const Color.fromRGBO(255, 243, 212, 1),
+};
+
+
