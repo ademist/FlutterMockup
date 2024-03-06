c3920515839fc22bbe2cc26ca3783e5ab36c7140
reichardcris <reichardcris@gmail.com>
2024-03-06 22:37:41 +0800

    Fix orientation of the layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:blog_post_app/config/router_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(
-  ProviderScope(child: RouterView())
-);
+void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(ProviderScope(child: RouterView()));
+  });
+
+  // runApp(
+  //   ProviderScope(child: RouterView())
+  // );
+}
