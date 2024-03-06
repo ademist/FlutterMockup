9b035a030f6c34a90f274ff0062b7b0e1aa3c2e7
reichardcris <reichardcris@gmail.com>
2024-03-06 22:41:25 +0800

    remove unwanted imports

--- lib/providers/global_provider.dart
+++ lib/providers/global_provider.dart
@@ -1,5 +1,3 @@
-import 'package:blog_post_app/apis/post_api.dart';
-import 'package:blog_post_app/models/post.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final screenIndexProvider = StateProvider<int>((ref) {
